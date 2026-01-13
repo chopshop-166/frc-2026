3f97660e4affbcaa3993907dddf4ded57595b95d
Matt Soucy <git@msoucy.me>
2026-01-13 17:56:26 -0500

    chore: Start importing relevant 2025 code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,69 +4,124 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.TimedRobot;
+import java.util.function.DoubleUnaryOperator;
+
+import org.littletonrobotics.junction.Logger;
+
+import com.chopshop166.chopshoplib.Autonomous;
+import com.chopshop166.chopshoplib.commands.CommandRobot;
+import com.chopshop166.chopshoplib.controls.ButtonXboxController;
+import com.pathplanner.lib.auto.AutoBuilder;
+
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.Commands;
+import frc.robot.maps.RobotMap;
 
-public class Robot extends TimedRobot {
-  private Command m_autonomousCommand;
+public final class Robot extends CommandRobot {
 
-  private final RobotContainer m_robotContainer;
+    private RobotMap map = getRobotMap(RobotMap.class, new RobotMap());
+    private ButtonXboxController driveController = new ButtonXboxController(0);
+    private ButtonXboxController copilotController = new ButtonXboxController(1);
 
-  public Robot() {
-    m_robotContainer = new RobotContainer();
-  }
+    // Helpers
+    final DoubleUnaryOperator driveScaler = getScaler(0.45, 0.25);
+    NetworkTableInstance ntinst = NetworkTableInstance.getDefault();
 
-  @Override
-  public void robotPeriodic() {
-    CommandScheduler.getInstance().run();
-  }
+    @Autonomous(name = "No Auto", defaultAuto = true)
+    public Command noAuto = Commands.none();
 
-  @Override
-  public void disabledInit() {}
+    private final SendableChooser<Command> autoChooser;
 
-  @Override
-  public void disabledPeriodic() {}
-
-  @Override
-  public void disabledExit() {}
-
-  @Override
-  public void autonomousInit() {
-    m_autonomousCommand = m_robotContainer.getAutonomousCommand();
-
-    if (m_autonomousCommand != null) {
-      CommandScheduler.getInstance().schedule(m_autonomousCommand);
+    public Robot() {
+        super();
+        registerNamedCommands();
+        autoChooser = AutoBuilder.buildAutoChooser();
     }
-  }
 
-  @Override
-  public void autonomousPeriodic() {}
+    @Override
+    public void robotInit() {
+        super.robotInit();
 
-  @Override
-  public void autonomousExit() {}
+        // Record metadata
+        Logger.recordMetadata("ProjectName", BuildConstants.MAVEN_NAME);
+        Logger.recordMetadata("BuildDate", BuildConstants.BUILD_DATE);
+        Logger.recordMetadata("GitSHA", BuildConstants.GIT_SHA);
+        Logger.recordMetadata("GitDate", BuildConstants.GIT_DATE);
+        Logger.recordMetadata("GitBranch", BuildConstants.GIT_BRANCH);
+        Logger.recordMetadata("GitDirty", switch (BuildConstants.DIRTY) {
+            case 0 ->
+                "All changes committed";
+            case 1 ->
+                "Uncomitted changes";
+            default ->
+                "Unknown";
+        });
+        Logger.recordMetadata("RobotMap", map.getClass().getName());
 
-  @Override
-  public void teleopInit() {
-    if (m_autonomousCommand != null) {
-      m_autonomousCommand.cancel();
+        map.setupLogging();
+
+        if (!isReal()) {
+            setUseTiming(false); // Run as fast as possible
+        }
+        // Start logging! No more data receivers, replay sources, or metadata values
+        // may be added.
+        Logger.start();
+
+        DriverStation.silenceJoystickConnectionWarning(true);
+
+        CommandScheduler.getInstance().onCommandInterrupt((oldCmd, newCmd) -> {
+            if (!DriverStation.isFMSAttached()) {
+                String newSub = "<NONE>";
+                String newName = "<NONE>";
+                if (newCmd.isPresent()) {
+                    newSub = newCmd.get().getSubsystem();
+                    newName = newCmd.get().getName();
+                }
+                System.out.println("Command interrupt: `" + oldCmd.getSubsystem() + "/" + oldCmd.getName() +
+                        "` -> `" + newSub + "/" + newName + "`");
+            }
+        });
+
     }
-  }
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void configureButtonBindings() {
+    }
 
-  @Override
-  public void teleopExit() {}
+    @Override
+    public void populateDashboard() {
+        SmartDashboard.putData("AutoChooser", autoChooser);
+    }
 
-  @Override
-  public void testInit() {
-    CommandScheduler.getInstance().cancelAll();
-  }
+    /**
+     * Use this to pass the autonomous command to the main {@link Robot} class.
+     *
+     * @return the command to run in autonomous
+     */
+    @Override
+    public Command getAutoCommand() {
+        return autoChooser.getSelected();
+    }
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void setDefaultCommands() {
+    }
 
-  @Override
-  public void testExit() {}
+    public DoubleUnaryOperator getScaler(double leftRange, double rightRange) {
+        return speed -> {
+            double leftTrigger = driveController.getLeftTriggerAxis();
+            double rightTrigger = driveController.getRightTriggerAxis();
+            double modifier = (rightRange * rightTrigger) - (leftRange * leftTrigger) + 0.75;
+            return modifier * speed;
+        };
+    }
+
+    private final void registerNamedCommands() {
+        //
+    }
 }
