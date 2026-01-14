578fcf7a9939c2e7a0d1fbb378b3c6a2ead3fe9f
Matt Soucy <git@msoucy.me>
2026-01-13 19:57:24 -0500

    chore: Add a note about the 2026 field

--- src/main/java/com/chopshop166/chopshoplib/maps/CameraSource.java
+++ src/main/java/com/chopshop166/chopshoplib/maps/CameraSource.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.geometry.Transform3d;
 /** Simplified wrapper around a camera with a pose estimator. */
 public class CameraSource {
     /** The default field, loaded for convenience. */
+    // TODO replace with the 2026 field when WPIlib makes it available
     public static final AprilTagFieldLayout DEFAULT_FIELD = AprilTagFieldLayout
             .loadField(AprilTagFields.k2025ReefscapeAndyMark);
     /** The camera object. */
