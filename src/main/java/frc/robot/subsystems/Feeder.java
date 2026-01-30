fd99f4d7c82044e254a107e87fe593ca7c9f7ee2
Abbott Davis <abbott.davis716@gmail.com>
2026-01-30 15:44:33 -0500

    Added Feeder

---
+++ src/main/java/frc/robot/subsystems/Feeder.java
@@ -0,0 +1,36 @@
+package frc.robot.subsystems;
+
+import static edu.wpi.first.wpilibj2.command.Commands.runOnce;
+import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.maps.subsystems.FeederMap;
+import frc.robot.maps.subsystems.FeederMap.Data;
+
+public class Feeder extends LoggedSubsystem<Data, FeederMap> {
+
+    private final double FEED_SPEED = .5;
+    private final double REVERSE_SPEED = .5;
+
+    public Feeder(FeederMap feederMap) {
+        super(new Data(), feederMap);
+    }
+
+    public Command Feed() {
+        return runOnce(() -> {
+            getData().feeder.setpoint = FEED_SPEED;
+        });
+    }
+
+    public Command Reverse() {
+        return runOnce(() -> {
+            getData().feeder.setpoint = REVERSE_SPEED;
+        });
+    }
+
+    @Override
+    public void safeState() {
+        getData().feeder.setpoint = 0;
+    }
+
+}
