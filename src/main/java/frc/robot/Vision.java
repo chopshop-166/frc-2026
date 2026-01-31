66cf1146820358f2146aed06893bee91db3a3922
Liam Murray <liammurray2015@icloud.com>
2026-01-30 20:04:37 -0500

    Started vision on kitbot

--- src/main/java/frc/robot/Vision.java
+++ src/main/java/frc/robot/Vision.java
@@ -2,7 +2,10 @@ package frc.robot;
 
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.Map;
+
+import org.littletonrobotics.junction.Logger;
 
 import com.chopshop166.chopshoplib.maps.CameraSource;
 
@@ -11,6 +14,7 @@ import edu.wpi.first.math.estimator.SwerveDrivePoseEstimator;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.geometry.Translation3d;
 
 public class Vision {
 
@@ -31,5 +35,22 @@ public class Vision {
                 }).thenComparing((Map.Entry<Integer, Pose2d> entry) -> {
                     return Math.abs(robotRotation.minus(entry.getValue().getRotation()).getRadians());
                 })).getKey();
+    }
+
+    public static Translation2d getHubCenter(boolean isBlueAlliance) {
+        Translation3d translation;
+        if (isBlueAlliance) {
+            Translation3d poseLeft = kTagLayout.getTagPose(10).get().getTranslation();
+            Translation3d poseRigth = kTagLayout.getTagPose(4).get().getTranslation();
+            Logger.recordOutput("Reef center", "Blue");
+            translation = poseLeft.plus(poseRigth).div(2);
+        } else {
+            Translation3d poseLeft = kTagLayout.getTagPose(20).get().getTranslation();
+            Translation3d poseRight = kTagLayout.getTagPose(26).get().getTranslation();
+            Logger.recordOutput("Reef center", "Red");
+            translation = poseLeft.plus(poseRight).div(2);
+        }
+
+        return translation.toTranslation2d();
     }
 }
