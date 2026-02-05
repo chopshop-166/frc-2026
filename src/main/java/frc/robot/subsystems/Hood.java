ca0813c790ede1e3fc45b8bd7c5e94deb45ca8e7
Abbott Davis <abbott.davis716@gmail.com>
2026-02-04 20:54:59 -0500

    Fixed issue

--- src/main/java/frc/robot/subsystems/Hood.java
+++ src/main/java/frc/robot/subsystems/Hood.java
@@ -1,11 +1,5 @@
 package frc.robot.subsystems;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Feet;
-import static edu.wpi.first.units.Units.FeetPerSecondPerSecond;
-import static edu.wpi.first.units.Units.RPM;
-import static edu.wpi.first.units.Units.Radians;
-
 import java.util.function.DoubleSupplier;
 
 import org.littletonrobotics.junction.Logger;
@@ -14,20 +8,9 @@ import com.chopshop166.chopshoplib.logging.LoggedSubsystem;
 
 import edu.wpi.first.math.controller.ProfiledPIDController;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.State;
-import edu.wpi.first.networktables.BooleanPublisher;
 import edu.wpi.first.networktables.DoubleSubscriber;
 import edu.wpi.first.networktables.NetworkTableInstance;
-import edu.wpi.first.units.AngleUnit;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.LinearAcceleration;
-import edu.wpi.first.units.measure.LinearVelocity;
-import edu.wpi.first.units.measure.Velocity;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.maps.subsystems.DeployerMap.DeployerPresets;
-import edu.wpi.first.units.Units;
-
 import frc.robot.maps.subsystems.HoodMap;
 import frc.robot.maps.subsystems.HoodMap.Data;
 
@@ -36,7 +19,6 @@ public class Hood extends LoggedSubsystem<Data, HoodMap> {
     NetworkTableInstance instance = NetworkTableInstance.getDefault();
     DoubleSubscriber distanceToTargetSub = instance.getDoubleTopic("Drive/Distance To Hub Ft").subscribe(0.0);
     DoubleSubscriber ShooterLinearVelocity = instance.getDoubleTopic("Shooter/Linear Velocity").subscribe(0.0);
-    double holdAngle = 0;
 
     public Hood(HoodMap hoodMap) {
         super(new Data(), hoodMap);
@@ -60,7 +42,7 @@ public class Hood extends LoggedSubsystem<Data, HoodMap> {
 
     public Command manualControl(DoubleSupplier joystick) {
         return run(() -> {
-            getData().motor.setpoint = joystick.getAsDouble();
+            getData().motor.setpoint = limits(joystick.getAsDouble());
         });
     }
 
